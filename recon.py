6ece8bb22dfa0a83a05911a0e799f664c420d463
1Mr12 <pubg.twitter12@gmail.com>
2023-11-09 00:45:14 +0200

    add BaseClas logSetup.py

--- recon.py
+++ recon.py
@@ -1,62 +1,24 @@
 #!/usr/bin/python3
 
 import argparse
-import os
-import subprocess
-import requests
 import re
-import logging
+from BaseClass import *
+
+
+logger = logSetup.log("recon","ReconLog.txt")
 
 class Recon:
     def __init__(self, FilePath):
-        self.logger = self.log()
+        self.logger = logger
         self.FilePath = FilePath
-        self.ScopeLinks = self.ReadFile(FilePath)
+        self.ScopeLinks = BaseClass.ReadFile(FilePath)
 
-
-    def ReadFile(self, Path):
-        if self.checkIfFileExist(Path):
-            try:
-                with open(Path, 'r') as file:
-                    Lines = [line.strip() for line in file.readlines() if line.strip()]
-                    return Lines
-            except:
-                self.logger.error("Error in reading file {}".format(Path))
-        else:
-            self.logger.error("File does not exist")
-
-
-    def checkIfFileExist(self, Path):
-        return os.path.isfile(Path)
-
-    def checkIfDir(self, dirPath):
-        return os.path.isdir(dirPath)
-
-    def ExcuteCommand(self, command):
-        return subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    def checkCommandResult(self, result):
-        if result.returncode == 0:
-            return True
-        else:
-            return False
-
-    def writeToFile(self, FileName, Data):
-        with open(FileName, "a") as file:
-            if isinstance(Data, list):
-                for i in Data:
-                    file.write(i+'\n')
-            else:
-                file.write(Data+'\n')
-
-    def sendRequest(self, url):
-        return requests.get(url)
    
     def FindFireWall(self):
-            if self.checkIfFileExist(self.FilePath):
+            if BaseClass.checkIfFileExist(self.FilePath):
                 command = "wafw00f -i {} -o wafwoof.txt".format(self.FilePath)
-                result = self.ExcuteCommand(command)
-                if self.checkCommandResult(result):
+                result = BaseClass.ExcuteCommand(command)
+                if BaseClass.checkCommandResult(result):
                     self.logger.info("Done finding firewall for all socpe result in waffwoff.txt")
                     return True
                 else:
@@ -65,8 +27,8 @@ class Recon:
 
     def FindFireWallForSingleUrl(self, Url):
         command = "wafw00f {}".format(Url)
-        result = self.ExcuteCommand(command)
-        if self.checkCommandResult(result):
+        result = BaseClass.ExcuteCommand(command)
+        if BaseClass.checkCommandResult(result):
             if "No WAF" in result.stdout:
                 return False
             elif "is behind" in result.stdout:
@@ -77,68 +39,37 @@ class Recon:
     def CreateScopeFoldars(self):
         for i in self.ScopeLinks:
             command  = "mkdir Scope/{}".format(i)
-            result = self.ExcuteCommand(command)
+            result = BaseClass.ExcuteCommand(command)
         else:
             self.logger.info("Done making foldars of the scope")
 
     def FindScopeHost(self):
-        if self.checkIfFileExist("host.txt"):
+        if BaseClass.checkIfFileExist("host.txt"):
             self.logger.info("found host.txt file")
             return True
         else:
             for i in self.ScopeLinks:
                 command = "host {} >> host.txt".format(i)
-                result = self.ExcuteCommand(command)
+                result = BaseClass.ExcuteCommand(command)
             else:
                 self.logger.info("Done find host ips")
                 return True
 
     def CollectIpsFromHost(self):
-        if self.checkIfFileExist("host.txt"):
+        if BaseClass.checkIfFileExist("host.txt"):
             command = "grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' host.txt > hostIps.txt"
-            self.ExcuteCommand(command)
+            BaseClass.ExcuteCommand(command)
             return True
         else:
             self.logger.error("host file not found")
             return False
 
-    def chekcTool(self, tool):
-        command = f"which {tool}"
-        result = self.ExcuteCommand(command)
-        if self.checkCommandResult(result):
-            return True
-        else:
-            self.logger.error(f"This {tool} is not installed")
-            return False
-
-
-
-    def log(self):
-        # .debug .info .warning .error .critical
-        # Create a logger for your script
-        logger = logging.getLogger('recon')
-        logger.setLevel(logging.DEBUG)  # Set the log level to DEBUG
-
-        # Create a file handler for writing log messages to a file
-        file_handler = logging.FileHandler('recon.log')
-        file_handler.setLevel(logging.DEBUG)  # Set the desired log level
-        file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        logger.addHandler(file_handler)  
-
-        # Create a stream handler for displaying log messages on the console
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.DEBUG)  # Set the desired log level
-        stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        logger.addHandler(stream_handler)
-        return logger
-
-
     def runSublist3r(self):
-        if self.chekcTool("sublist3r"):
+        if BaseClass.chekcTool("sublist3r"):
             for i in self.ScopeLinks:
-                if self.checkIfDir(i):
+                if BaseClass.checkIfDir(i):
                     command = f"sublist3r -d {i} -o {i}/{i}.sublist3r.txt"
-                    result = self.ExcuteCommand(command)
+                    result = BaseClass.ExcuteCommand(command)
                 else:
                     self.logger.error(f"can't find {i} Directory")
             else:
@@ -149,11 +80,11 @@ class Recon:
 
 
     def runSubfinder(self):
-        if self.chekcTool("subfinder"):
+        if BaseClass.chekcTool("subfinder"):
             for i in self.ScopeLinks:
-                if self.checkIfDir(i):
+                if BaseClass.checkIfDir(i):
                     command = f"subfinder -d {i} -o {i}/{i}.subfinder.txt"
-                    result = self.ExcuteCommand(command)
+                    result = BaseClass.ExcuteCommand(command)
                 else:
                     self.logger.error(f"can't find {i} Directory")
             else:
@@ -163,13 +94,13 @@ class Recon:
             return False
     
     def runAmass(self):
-        if self.chekcTool("amass"):
+        if BaseClass.chekcTool("amass"):
             for i in self.ScopeLinks:
-                if self.checkIfDir(i):
+                if BaseClass.checkIfDir(i):
                     command = f"enum --passive -d {i} -o {i}/{i}.passiveAmass.txt"
-                    result = self.ExcuteCommand(command)
+                    result = BaseClass.ExcuteCommand(command)
                     command = f"enum --active -d {i} -o {i}/{i}.ActiveAmass.txt"
-                    result = self.ExcuteCommand(command)
+                    result = BaseClass.ExcuteCommand(command)
                 else:
                     self.logger.error(f"can't find {i} Directory")
             else:
@@ -179,11 +110,11 @@ class Recon:
             return False
 
     def runAssetfinder(self):
-        if self.chekcTool("assetfinder"):
+        if BaseClass.chekcTool("assetfinder"):
             for i in self.ScopeLinks:
-                if self.checkIfDir(i):
+                if BaseClass.checkIfDir(i):
                     command = f"assetfinder {i} > {i}/{i}.assetfinder.txt"
-                    result = self.ExcuteCommand(command)
+                    result = BaseClass.ExcuteCommand(command)
                 else:
                     self.logger.error(f"can't find {i} Directory")
             else:
@@ -194,14 +125,14 @@ class Recon:
 
     def runCrtsh(self):
         for i in self.ScopeLinks:
-            if self.checkIfDir(i):
+            if BaseClass.checkIfDir(i):
                 crtUrlSite = f'https://crt.sh/?q=%.{i}&output=json'
-                response = self.sendRequest(crtUrlSite)
+                response = BaseClass.sendRequest(crtUrlSite)
                 if response.status_code == 200:
                     data = response.text
                     subdomains = set(re.findall(r'\b(?:[a-zA-Z0-9.-]+\.)*' + re.escape(i) + r'\b', data))
                     FileName = f"{i}/{i}.crtSh.txt"
-                    self.writeToFile(FileName, subdomains)
+                    BaseClass.writeToFile(FileName, subdomains)
                 else:
                     self.logger.error(f'Error in geting crt.sh: {response.status_code}')
             else:
@@ -209,11 +140,11 @@ class Recon:
 
 
     def runWaybacruls(self):
-        if self.chekcTool("waybackurls"):
+        if BaseClass.chekcTool("waybackurls"):
             for i in self.ScopeLinks:
-                if self.checkIfDir(i):
+                if BaseClass.checkIfDir(i):
                     command = f"waybackurls {i} > {i}/{i}.waybackurls.txt"
-                    result = self.ExcuteCommand(command)
+                    result = BaseClass.ExcuteCommand(command)
                 else:
                     self.logger.error(f"can't find {i} Directory")
             else:
@@ -225,17 +156,17 @@ class Recon:
     # This recursive is useless thanks to MohamedSamehMohamed you can just check for file existence then create it first
     def findRealIpScope(self):
         # generate Real Ips from hostIp.txt
-        if self.checkIfFileExist("hostIps.txt"):
-            for i in self.ReadFile("hostIps.txt"):
+        if BaseClass.checkIfFileExist("hostIps.txt"):
+            for i in BaseClass.ReadFile("hostIps.txt"):
                 isFireWall = self.isFireWallIp(i)
                 if isFireWall:
                     continue
                 else:
-                    self.writeToFile("scopeRealIps.txt",i)
+                    BaseClass.writeToFile("scopeRealIps.txt",i)
             else:
                 self.logger.info("done finding real ip")
         else:
-            if self.checkIfFileExist("host.txt"):
+            if BaseClass.checkIfFileExist("host.txt"):
                 self.CollectIpsFromHost()
                 self.findRealIpScope()
             else:
@@ -246,10 +177,10 @@ class Recon:
 
     def runMasscanOnScope(self):
         # Don't forget to check if it's a firewall or a site ip
-        if self.checkIfFileExist("scopeRealIps.txt"):
-            for i in self.ReadFile("scopeRealIps.txt"):
+        if BaseClass.checkIfFileExist("scopeRealIps.txt"):
+            for i in BaseClass.ReadFile("scopeRealIps.txt"):
                 command = f"sudo msudo masscan -p- {i} > {i}.massScan.txt"
-                self.ExcuteCommand(command)
+                BaseClass.ExcuteCommand(command)
                 self.logger.info(f"done massscan on {i}")
             else:
                 self.logger.info("Done Mass scan ") 
@@ -259,8 +190,8 @@ class Recon:
 
     def runNmapOnScope(self):
         # Don't forget to check if it's a firewall or a site ip
-        if self.checkIfFileExist("scopeRealIps.txt"):
-            for i in self.ReadFile("scopeRealIps.txt"):
+        if BaseClass.checkIfFileExist("scopeRealIps.txt"):
+            for i in BaseClass.ReadFile("scopeRealIps.txt"):
                 command = f"sudo nmap -Pn -sS -sC -sV -F {i} -o {i}.namp.txt"
             else:
                 self.logger.info("Done Nmap")
